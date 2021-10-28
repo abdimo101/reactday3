a4f4a8a6e3a97a7b9daf30a92c9560370f3dfc86
Abdi Abdulle <cph-aa394@cphbusiness.dk>
2021-10-28 02:07:38 +0200

    handling inputs done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import ContactForm from './ContactForm';
+import ReservationForm from './FormDemoMultiple';
 
 function App() {
   return (
     <div>
       <h1>Onsdagsdemoer</h1>
       <ContactForm/>
+      <h2>Handling inputs</h2>
+      <ReservationForm/>
     </div>
   );
 }
